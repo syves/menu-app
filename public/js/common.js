aafa5bd4a1e679dbd2294fdddf4f51520fb8cc17
Shakrah Yves <7shakrahs@gmail.com>
2013-12-02 23:20:43 -0800

    ignore inherited property names

--- public/js/common.js
+++ public/js/common.js
@@ -5,7 +5,7 @@ MenuApp.store = {
     localStorage[key] = JSON.stringify(value);
   }, 
   get: function(key, default_) {
-    if (key in localStorage){
+    if (Object.prototype.hasOwnProperty.call(localStorage, key)) {
       return JSON.parse(localStorage[key]);
     } else {
       return default_;
