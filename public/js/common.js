bdd91af4543543cac0e49a79cd2b8b50464274cf
Shakrah Yves <7shakrahs@gmail.com>
2013-12-08 22:52:26 -0800

    deleted strip-obsolete

--- public/js/common.js
+++ public/js/common.js
@@ -15,19 +15,3 @@ MenuApp.store = {
 MenuApp.menus = {}
 MenuApp.diets = {}
 MenuApp.cuisines = {}
-
-
-MenuApp._printCuisine = function(menuIds) {
-  var ingredients = {};
-  for (var i = 0; i < menuIds.length; i += 1) {
-    var menuId = menuIds[i];
-    var menu = MenuApp.menus[menuId];
-    for (var name in menu) {
-      for (var j = 0; j < menu[name].length; j += 1) {
-        var ingredient = menu[name][j];
-        ingredients[ingredient] = 1;
-      }
-    }
-  }
-  return JSON.stringify(Object.keys(ingredients).sort(), null, 2);
-};
